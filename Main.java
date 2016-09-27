a51bc0decb4a0ea53a8f3acb888e56ff0e1651c8
rob329 <robert_bolt@utexas.edu>
2016-09-26 20:15:34 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 /* WORD LADDER Main.java
  * EE422C Project 3 submission by
  * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
+ * <Robert Bolt>
+ * <rob329>
  * <Student1 5-digit Unique No.>
  * <Student2 Name>
  * <Student2 EID>
  * <Student2 5-digit Unique No.>
  * Slip days used: <0>
- * Git URL:
+ * Git URL: 
  * Fall 2016
  */
 
